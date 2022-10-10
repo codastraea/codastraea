333c2c2e38a99cd8338be9c2b197d3f58d9b8cc8
Simon Bourne <simonbourne@gmail.com>
2022-10-10 16:10:14 +0100

    Pass `RunTracer` down to UI client handler

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,4 +1,8 @@
-use std::{sync::RwLock, thread, time::Duration};
+use std::{
+    sync::{Arc, RwLock},
+    thread,
+    time::Duration,
+};
 
 use axum::{
     extract::{
@@ -21,10 +25,12 @@ use tokio::time::sleep;
 #[tokio::main]
 async fn main() {
     let lib = Library::link(parse(CODE).unwrap());
-    let tracer = RwLock::new(RunTracer::new());
+    // Unfortunately we need to use an `Arc` as axum requires `'static` lifetimes on
+    // closures/futures.
+    let tracer = Arc::new(RwLock::new(RunTracer::new()));
 
     thread::scope(|scope| {
-        scope.spawn(|| ui(&tracer));
+        scope.spawn(|| ui(tracer.clone()));
         scope.spawn(|| loop {
             run(&lib, &tracer)
         });
@@ -32,8 +38,9 @@ async fn main() {
 }
 
 #[tokio::main]
-async fn ui(_tracer: &RwLock<RunTracer>) {
-    let app = Router::new().route("/", get(ws_handler));
+async fn ui(tracer: Arc<RwLock<RunTracer>>) {
+    let handler = move |ws, user_agent| async move { ws_handler(tracer, ws, user_agent).await };
+    let app = Router::new().route("/", get(handler));
     Server::bind(&"0.0.0.0:9090".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -41,6 +48,7 @@ async fn ui(_tracer: &RwLock<RunTracer>) {
 }
 
 async fn ws_handler(
+    tracer: Arc<RwLock<RunTracer>>,
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
 ) -> impl IntoResponse {
@@ -48,10 +56,10 @@ async fn ws_handler(
         println!("`{}` connected", user_agent.as_str());
     }
 
-    ws.on_upgrade(handle_socket)
+    ws.on_upgrade(move |socket| handle_socket(tracer, socket))
 }
 
-async fn handle_socket(mut socket: WebSocket) {
+async fn handle_socket(_tracer: Arc<RwLock<RunTracer>>, mut socket: WebSocket) {
     println!("Upgraded to websocket");
 
     let mut count = 0;
