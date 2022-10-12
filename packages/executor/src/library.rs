fc5e6985208804142d22deac151fa6a5edcba100
Simon Bourne <simonbourne@gmail.com>
2022-10-12 12:07:22 +0100

    Docs

--- packages/executor/src/library.rs
+++ packages/executor/src/library.rs
@@ -8,6 +8,10 @@ pub struct Library {
 }
 
 impl Library {
+    /// Constructor
+    ///
+    /// Translate all `String` function id's to a [`FunctionId`] that is fast to
+    /// lookup
     pub fn link(module: Module) -> Self {
         let mut id_map = IdMap::new();
         let mut main_id = None;
@@ -34,18 +38,33 @@ impl Library {
         }
     }
 
+    /// Lookup a function id
+    ///
+    /// Any [`FunctionId`]'s in the return value will be valid lookup with this
+    /// function.
+    ///
+    /// # Panic
+    ///
+    /// If `id` was not found.
     pub fn lookup(&self, id: FunctionId) -> &Function<FunctionId> {
         &self.lookup_map[id.0]
     }
 
+    /// Lookup a function called "main"
+    ///
+    /// Returns `None` if not found.
     pub fn main(&self) -> Option<&Function<FunctionId>> {
         self.main_id.map(|main| self.lookup(main))
     }
 
+    /// The id of a function called "main"
+    ///
+    /// Returns `None` if there was no "main" function.
     pub fn main_id(&self) -> Option<FunctionId> {
         self.main_id
     }
 }
 
+/// An id for a function that is fast to lookup.
 #[derive(Eq, PartialEq, Hash, Copy, Clone, Serialize, Deserialize, Debug)]
 pub struct FunctionId(usize);
