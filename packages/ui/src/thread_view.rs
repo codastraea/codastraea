729eaac474f1c4e86dfd725c609d3e52e09e5ef8
Simon Bourne <simonbourne@gmail.com>
2022-10-26 16:41:07 +0100

    Change item order

--- packages/ui/src/thread_view.rs
+++ packages/ui/src/thread_view.rs
@@ -119,140 +119,6 @@ fn function_node(
     }
 }
 
-fn expandable_node<Elem>(
-    type_name: &str,
-    style: ButtonStyle,
-    run_state: impl Signal<Item = RunState> + 'static,
-    is_last: bool,
-    is_expanded: Mutable<bool>,
-    mut expanded: impl FnMut() -> Elem + 'static,
-) -> Element
-where
-    Elem: Into<Element>,
-{
-    node_column(
-        button_group(type_name)
-            .shadow(Shadow::Medium)
-            .dropdown(item_dropdown(type_name, style, run_state))
-            .button(zoom_button(&is_expanded, style)),
-        is_last,
-    )
-    .child(
-        row()
-            .align_items(Align::Start)
-            .class(css::EXPANDABLE_NODE)
-            .animated_expand(
-                move || div().speech_bubble().child(expanded().into()),
-                is_expanded,
-            ),
-    )
-    .into()
-}
-
-fn node_column(header: impl Into<Element>, is_last: bool) -> DivBuilder {
-    let main = row().align_items(Align::Center).child(header.into());
-
-    column().align_items(Align::Stretch).child(if is_last {
-        main
-    } else {
-        let horizontal_line = div().class(css::HORIZONTAL_LINE);
-        let arrow_head = div()
-            .class(css::ARROW_HEAD_RIGHT)
-            .background_colour(Colour::Secondary);
-        main.child(horizontal_line).child(arrow_head)
-    })
-}
-
-fn item_dropdown(
-    name: &str,
-    style: ButtonStyle,
-    run_state: impl Signal<Item = RunState> + 'static,
-) -> DropdownBuilder {
-    let run_state = run_state.map(|run_state| {
-        match run_state {
-            RunState::NotRun => Icon::circle().colour(Colour::Secondary),
-            RunState::Running => Icon::play_circle_fill().colour(Colour::Primary),
-            RunState::Successful | RunState::PredicateSuccessful(true) => {
-                Icon::check_circle_fill().colour(Colour::Success)
-            }
-            RunState::PredicateSuccessful(false) => Icon::circle_fill().colour(Colour::Success),
-            RunState::Failed => Icon::exclamation_triangle_fill().colour(Colour::Danger),
-        }
-        .margin_on_side((Some(Size::Size2), Side::End))
-    });
-
-    dropdown(
-        icon_button("button", Sig(run_state), style).text(name),
-        dropdown_menu().children([dropdown_item("Run"), dropdown_item("Pause")]),
-    )
-}
-
-fn dropdown_item(name: &str) -> ABuilder {
-    a().href("#").text(name)
-}
-
-fn call<'a>(
-    name: FunctionId,
-    args: &'a [Expression<FunctionId>],
-    is_last: bool,
-    call_stack: CallStack,
-    view_state: &'a ThreadViewState,
-) -> impl Iterator<Item = Element> + 'a {
-    args.iter()
-        .enumerate()
-        .flat_map({
-            clone!(mut call_stack);
-
-            move |(arg_index, arg)| {
-                // TODO: Push and pop call stack for efficiency
-                call_stack.push(StackFrame::Argument(arg_index));
-                expression(arg, false, &call_stack, view_state)
-            }
-        })
-        .chain(iter::once(function_node(
-            name, is_last, call_stack, view_state,
-        )))
-}
-
-fn body_statements<'a>(
-    body: impl Iterator<Item = &'a Statement<FunctionId>>,
-    call_stack: &'a CallStack,
-    view_state: &'a ThreadViewState,
-) -> impl Iterator<Item = Element> + 'a {
-    let body: Vec<_> = body
-        .enumerate()
-        .filter(|(_index, stmt)| statement_is_expandable(stmt))
-        .collect();
-    assert!(!body.is_empty());
-    let last_index = body.len() - 1;
-
-    body.into_iter()
-        .enumerate()
-        .flat_map(move |(index, (stmt_index, statement))| {
-            let is_last = index == last_index;
-            clone!(mut call_stack);
-            call_stack.push(StackFrame::Statement(stmt_index));
-
-            match statement {
-                Statement::Pass => Vec::new(),
-                Statement::Expression(expr) => expression(expr, is_last, &call_stack, view_state),
-                Statement::If {
-                    condition,
-                    then_block,
-                    else_block,
-                } => vec![if_node(
-                    condition.clone(),
-                    then_block.clone(),
-                    else_block.clone(),
-                    is_last,
-                    &call_stack,
-                    view_state,
-                )],
-            }
-            .into_iter()
-        })
-}
-
 fn if_node(
     condition: Arc<Expression<FunctionId>>,
     then_block: Arc<Body<FunctionId>>,
@@ -290,35 +156,6 @@ fn if_node(
                 )
         },
     )
-}
-
-fn branch_body(
-    condition: Option<&Arc<Expression<FunctionId>>>,
-    body: &Arc<Body<FunctionId>>,
-    nested_block_index: usize,
-    call_stack: &CallStack,
-    view_state: &ThreadViewState,
-) -> Element {
-    let is_expandable = is_expandable(body);
-    let condition = condition_node(
-        condition,
-        nested_block_index,
-        !is_expandable,
-        call_stack,
-        view_state,
-    );
-
-    clone!(mut call_stack);
-    call_stack.push(StackFrame::NestedBlock(nested_block_index));
-
-    let body_elem = row().align_items(Align::Stretch).child(condition);
-
-    if is_expandable {
-        body_elem.children(body_statements(body.iter(), &call_stack, view_state))
-    } else {
-        body_elem
-    }
-    .into()
 }
 
 fn condition_node(
@@ -370,6 +207,65 @@ fn condition_leaf_node(
     leaf_node(name, CONDITION_STYLE, run_state, is_last)
 }
 
+fn branch_body(
+    condition: Option<&Arc<Expression<FunctionId>>>,
+    body: &Arc<Body<FunctionId>>,
+    nested_block_index: usize,
+    call_stack: &CallStack,
+    view_state: &ThreadViewState,
+) -> Element {
+    let is_expandable = is_expandable(body);
+    let condition = condition_node(
+        condition,
+        nested_block_index,
+        !is_expandable,
+        call_stack,
+        view_state,
+    );
+
+    clone!(mut call_stack);
+    call_stack.push(StackFrame::NestedBlock(nested_block_index));
+
+    let body_elem = row().align_items(Align::Stretch).child(condition);
+
+    if is_expandable {
+        body_elem.children(body_statements(body.iter(), &call_stack, view_state))
+    } else {
+        body_elem
+    }
+    .into()
+}
+
+fn expandable_node<Elem>(
+    type_name: &str,
+    style: ButtonStyle,
+    run_state: impl Signal<Item = RunState> + 'static,
+    is_last: bool,
+    is_expanded: Mutable<bool>,
+    mut expanded: impl FnMut() -> Elem + 'static,
+) -> Element
+where
+    Elem: Into<Element>,
+{
+    node_column(
+        button_group(type_name)
+            .shadow(Shadow::Medium)
+            .dropdown(item_dropdown(type_name, style, run_state))
+            .button(zoom_button(&is_expanded, style)),
+        is_last,
+    )
+    .child(
+        row()
+            .align_items(Align::Start)
+            .class(css::EXPANDABLE_NODE)
+            .animated_expand(
+                move || div().speech_bubble().child(expanded().into()),
+                is_expanded,
+            ),
+    )
+    .into()
+}
+
 fn leaf_node(
     name: &str,
     style: ButtonStyle,
@@ -381,6 +277,48 @@ fn leaf_node(
         is_last,
     )
     .into()
+}
+
+fn node_column(header: impl Into<Element>, is_last: bool) -> DivBuilder {
+    let main = row().align_items(Align::Center).child(header.into());
+
+    column().align_items(Align::Stretch).child(if is_last {
+        main
+    } else {
+        let horizontal_line = div().class(css::HORIZONTAL_LINE);
+        let arrow_head = div()
+            .class(css::ARROW_HEAD_RIGHT)
+            .background_colour(Colour::Secondary);
+        main.child(horizontal_line).child(arrow_head)
+    })
+}
+
+fn item_dropdown(
+    name: &str,
+    style: ButtonStyle,
+    run_state: impl Signal<Item = RunState> + 'static,
+) -> DropdownBuilder {
+    let run_state = run_state.map(|run_state| {
+        match run_state {
+            RunState::NotRun => Icon::circle().colour(Colour::Secondary),
+            RunState::Running => Icon::play_circle_fill().colour(Colour::Primary),
+            RunState::Successful | RunState::PredicateSuccessful(true) => {
+                Icon::check_circle_fill().colour(Colour::Success)
+            }
+            RunState::PredicateSuccessful(false) => Icon::circle_fill().colour(Colour::Success),
+            RunState::Failed => Icon::exclamation_triangle_fill().colour(Colour::Danger),
+        }
+        .margin_on_side((Some(Size::Size2), Side::End))
+    });
+
+    dropdown(
+        icon_button("button", Sig(run_state), style).text(name),
+        dropdown_menu().children([dropdown_item("Run"), dropdown_item("Pause")]),
+    )
+}
+
+fn dropdown_item(name: &str) -> ABuilder {
+    a().href("#").text(name)
 }
 
 fn zoom_button(
@@ -406,6 +344,29 @@ fn zoom_button(
     })
 }
 
+fn call<'a>(
+    name: FunctionId,
+    args: &'a [Expression<FunctionId>],
+    is_last: bool,
+    call_stack: CallStack,
+    view_state: &'a ThreadViewState,
+) -> impl Iterator<Item = Element> + 'a {
+    args.iter()
+        .enumerate()
+        .flat_map({
+            clone!(mut call_stack);
+
+            move |(arg_index, arg)| {
+                // TODO: Push and pop call stack for efficiency
+                call_stack.push(StackFrame::Argument(arg_index));
+                expression(arg, false, &call_stack, view_state)
+            }
+        })
+        .chain(iter::once(function_node(
+            name, is_last, call_stack, view_state,
+        )))
+}
+
 fn expression(
     expr: &Expression<FunctionId>,
     is_last: bool,
@@ -418,6 +379,45 @@ fn expression(
             call(*name, args, is_last, call_stack.clone(), view_state).collect()
         }
     }
+}
+
+fn body_statements<'a>(
+    body: impl Iterator<Item = &'a Statement<FunctionId>>,
+    call_stack: &'a CallStack,
+    view_state: &'a ThreadViewState,
+) -> impl Iterator<Item = Element> + 'a {
+    let body: Vec<_> = body
+        .enumerate()
+        .filter(|(_index, stmt)| statement_is_expandable(stmt))
+        .collect();
+    assert!(!body.is_empty());
+    let last_index = body.len() - 1;
+
+    body.into_iter()
+        .enumerate()
+        .flat_map(move |(index, (stmt_index, statement))| {
+            let is_last = index == last_index;
+            clone!(mut call_stack);
+            call_stack.push(StackFrame::Statement(stmt_index));
+
+            match statement {
+                Statement::Pass => Vec::new(),
+                Statement::Expression(expr) => expression(expr, is_last, &call_stack, view_state),
+                Statement::If {
+                    condition,
+                    then_block,
+                    else_block,
+                } => vec![if_node(
+                    condition.clone(),
+                    then_block.clone(),
+                    else_block.clone(),
+                    is_last,
+                    &call_stack,
+                    view_state,
+                )],
+            }
+            .into_iter()
+        })
 }
 
 const CONDITION_STYLE: ButtonStyle = ButtonStyle::Solid(Colour::Info);
