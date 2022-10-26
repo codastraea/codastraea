dc3c4f82e7a244612251fec11bb2c80880a8b857
Simon Bourne <simonbourne@gmail.com>
2022-10-26 13:53:34 +0100

    Fix size animation

--- packages/ui/src/animation.rs
+++ packages/ui/src/animation.rs
@@ -1,4 +1,4 @@
-use std::{cell::Cell, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
 use futures_signals::signal::{Mutable, SignalExt};
 use silkenweb::{
@@ -25,35 +25,39 @@ impl AnimatedExpand for DivBuilder {
     fn animated_expand<Elem>(
         self,
         mut child: impl FnMut() -> Elem + 'static,
-        expanded: Mutable<bool>,
+        is_expanded: Mutable<bool>,
     ) -> Self
     where
         Elem: Into<Element>,
     {
         let style = Mutable::new(Some("".to_owned()));
-        let initial_bounds: Rc<Cell<Option<DomRect>>> = Rc::new(Cell::new(None));
+        let delayed_is_expanded = Mutable::new(is_expanded.get());
+        let initial_bounds: Rc<RefCell<Option<DomRect>>> = Rc::new(RefCell::new(None));
+        let element = self.handle().dom_element();
+        let delayed_is_expanded_signal = delayed_is_expanded.signal();
 
         self.style(Sig(style.signal_cloned()))
-            .optional_child(Sig(expanded
-                .signal()
-                .map(move |expanded| expanded.then(|| child().into()))))
+            .optional_child(Sig(is_expanded.signal().map({
+                clone!(initial_bounds);
+                move |expanded| {
+                    initial_bounds.replace(Some(element.get_bounding_client_rect()));
+                    delayed_is_expanded.set(expanded);
+                    expanded.then(|| child().into())
+                }
+            })))
             .on_transitionend({
                 clone!(style);
                 move |_, _| style.set(None)
             })
-            .effect_signal(expanded.signal(), move |elem, expanded| {
+            .effect_signal(delayed_is_expanded_signal, move |elem, expanded| {
                 let final_bounds = elem.get_bounding_client_rect();
+                let limit = if expanded { "max" } else { "min" };
+                set_style_size(&style, limit, initial_bounds.borrow().as_ref().unwrap());
 
-                if let Some(initial_bounds) = initial_bounds.replace(Some(final_bounds.clone())) {
-                    let limit = if expanded { "max" } else { "min" };
-
-                    set_style_size(&style, limit, &initial_bounds);
-
-                    on_animation_frame({
-                        clone!(style);
-                        move || set_style_size(&style, limit, &final_bounds)
-                    })
-                }
+                on_animation_frame({
+                    clone!(style);
+                    move || set_style_size(&style, limit, &final_bounds)
+                })
             })
     }
 }
