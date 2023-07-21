1b7745726d5c2d47ab586c42de554c1cc190d02b
Simon Bourne <simonbourne@gmail.com>
2023-07-21 17:31:49 +0100

    Call stack ordering: earlier < later

--- packages/executor/src/run.rs
+++ packages/executor/src/run.rs
@@ -1,20 +1,42 @@
-use std::collections::BTreeMap;
+use std::{cmp::Ordering, collections::BTreeMap};
 
 use serde::{Deserialize, Serialize};
 
 use crate::library::FunctionId;
 
+// The order of the enum variants is important, as we rely on later call stacks
+// to be greater than earlier ones.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum StackFrame {
-    Function(FunctionId),
     Statement(usize),
     Argument(usize),
-    NestedBlock(usize),
+    Function(FunctionId),
     BlockPredicate(usize),
+    NestedBlock(usize),
 }
 
-#[derive(Debug, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub struct CallStack(Vec<StackFrame>);
+
+impl Ord for CallStack {
+    fn cmp(&self, other: &Self) -> Ordering {
+        for (i, j) in self.0.iter().zip(other.0.iter()) {
+            let cmp = i.cmp(j);
+
+            if cmp != Ordering::Equal {
+                return cmp;
+            }
+        }
+
+        other.0.len().cmp(&self.0.len())
+    }
+}
+
+impl PartialOrd for CallStack {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 // TODO: We need a `block_index` at each level to disambiguate.
 impl CallStack {
@@ -79,6 +101,10 @@ impl ThreadRunState {
     }
 
     fn pop(&mut self, run_state: RunState) {
+        if let Some((last, _)) = self.history.last_key_value() {
+            assert!(last < &self.current);
+        }
+
         self.history.insert(self.current.clone(), run_state);
         self.current.pop();
     }
