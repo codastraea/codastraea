99a1fd3f4789aa5b1dee326bd0131bf19ebecaaa
Simon Bourne <simonbourne@gmail.com>
2025-05-09 00:07:32 +0100

    More complete UI5 tabs

--- packages/ui5/src/tabs.rs
+++ packages/ui5/src/tabs.rs
@@ -1,11 +1,27 @@
 mod elements {
     use silkenweb::{custom_html_element, parent_element};
 
-    use super::Design;
+    use super::{BackgroundDesign, Design, Layout, OverflowMode};
+    use crate::icon;
 
     custom_html_element!(
         ui5_tabcontainer = {
             dom_type: web_sys::HtmlElement;
+
+            attributes {
+                collapsed: bool,
+                tab_layout: Layout,
+                overflow_mode: OverflowMode,
+                header_background_design: BackgroundDesign,
+                content_background_design: BackgroundDesign,
+                no_auto_selection: bool,
+            };
+
+            events {
+                tab_select: web_sys::CustomEvent,
+                move_over: web_sys::CustomEvent,
+                r#move: web_sys::CustomEvent,
+            };
         }
     );
 
@@ -18,6 +34,7 @@ mod elements {
                 text: String,
                 disabled: bool,
                 additional_text: String,
+                icon: icon::Name,
                 design: Design,
                 selected: bool,
             };
@@ -25,13 +42,70 @@ mod elements {
     );
 
     parent_element!(ui5_tab);
+
+    custom_html_element!(
+        ui5_tab_separator = {
+            dom_type: web_sys::HtmlElement;
+        }
+    );
 }
 
 pub use elements::{
-    ui5_tab as tab, ui5_tabcontainer as container, Ui5Tab as Tab, Ui5Tabcontainer as Container,
+    ui5_tab as tab, ui5_tab_separator as separator, ui5_tabcontainer as container, Ui5Tab as Tab,
+    Ui5TabSeparator as Separator, Ui5Tabcontainer as Container,
 };
 use silkenweb::attribute::{AsAttribute, Attribute};
 use strum::AsRefStr;
+
+// TODO: Derive macro for StrAttribute that calls `as_ref`
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr)]
+pub enum Layout {
+    Inline,
+    Standard,
+}
+
+impl Attribute for Layout {
+    type Text<'a> = &'a str;
+
+    fn text(&self) -> Option<Self::Text<'_>> {
+        Some(self.as_ref())
+    }
+}
+
+impl AsAttribute<Layout> for Layout {}
+
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr)]
+pub enum OverflowMode {
+    End,
+    StartAndEnd,
+}
+
+impl Attribute for OverflowMode {
+    type Text<'a> = &'a str;
+
+    fn text(&self) -> Option<Self::Text<'_>> {
+        Some(self.as_ref())
+    }
+}
+
+impl AsAttribute<OverflowMode> for OverflowMode {}
+
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr)]
+pub enum BackgroundDesign {
+    Solid,
+    Transparent,
+    Translucent,
+}
+
+impl Attribute for BackgroundDesign {
+    type Text<'a> = &'a str;
+
+    fn text(&self) -> Option<Self::Text<'_>> {
+        Some(self.as_ref())
+    }
+}
+
+impl AsAttribute<BackgroundDesign> for BackgroundDesign {}
 
 #[derive(Copy, Clone, PartialEq, Eq, AsRefStr)]
 pub enum Design {
