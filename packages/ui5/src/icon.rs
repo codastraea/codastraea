b58fc4d5fc47b5ba8433aff9c5d8df509ffff5b4
Simon Bourne <simonbourne@gmail.com>
2025-05-10 18:56:17 +0100

    Simplify `Icon`

--- packages/ui5/src/icon.rs
+++ packages/ui5/src/icon.rs
@@ -1,6 +1,6 @@
 use silkenweb::{
     attribute::{AsAttribute, Attribute},
-    StrAttribute,
+    custom_html_element, StrAttribute,
 };
 use strum::AsRefStr;
 
@@ -8,27 +8,19 @@ pub mod base;
 pub mod business_suite;
 pub mod tnt;
 
-mod elements {
-    use silkenweb::custom_html_element;
+custom_html_element!(
+    element("ui5-icon") = {
+        dom_type: web_sys::HtmlElement;
 
-    use super::{Design, Mode, Name};
-
-    custom_html_element!(
-        ui5_icon = {
-            dom_type: web_sys::HtmlElement;
-
-            attributes {
-                design: Design,
-                name: Name,
-                accessible_name: String,
-                show_tooltip: String,
-                mode: Mode,
-            };
-        }
-    );
-}
-
-pub use elements::{ui5_icon as element, Ui5Icon as Element};
+        attributes {
+            design: Design,
+            name: Name,
+            accessible_name: String,
+            show_tooltip: String,
+            mode: Mode,
+        };
+    }
+);
 
 #[derive(Copy, Clone, Eq, PartialEq, AsRefStr, StrAttribute)]
 pub enum Design {
