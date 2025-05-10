9049d9e0c615ddf7d0e2b80dbbbd01139c2d42ce
Simon Bourne <simonbourne@gmail.com>
2025-05-10 19:25:58 +0100

    UI5: Add tree

--- packages/ui5/src/lib.rs
+++ packages/ui5/src/lib.rs
@@ -1,2 +1,23 @@
+use silkenweb::StrAttribute;
+use strum::AsRefStr;
+
 pub mod icon;
 pub mod tab;
+pub mod tree;
+
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr, StrAttribute)]
+pub enum TextState {
+    None,
+    Positive,
+    Critical,
+    Negative,
+    Information,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr, StrAttribute)]
+pub enum ItemType {
+    Inactive,
+    Active,
+    Detail,
+    Navigation,
+}
---
+++ packages/ui5/src/tree.rs
@@ -0,0 +1,63 @@
+use silkenweb::{custom_html_element, parent_element, StrAttribute};
+use strum::AsRefStr;
+
+use crate::{icon, ItemType, TextState};
+
+custom_html_element!(
+    container("ui5-tree") = {
+        dom_type: web_sys::HtmlElement;
+
+        attributes {
+            selection_mode: SelectionMode,
+            no_data_text: String,
+            header_text: String,
+            footer_text: String,
+            accessible_name: String,
+            accessible_name_ref: String,
+            accessible_description: String,
+            accessible_description_ref: String,
+        };
+    }
+);
+
+parent_element!(container);
+
+custom_html_element!(
+    item("ui5-tree-item") = {
+        dom_type: web_sys::HtmlElement;
+
+        attributes {
+            text: String,
+            additional_text: String,
+            icon: icon::Name,
+            expanded: bool,
+            movable: bool,
+            indeterminate: bool,
+            has_children: bool,
+            additional_text_state: TextState,
+            accessible_name: String,
+            r#type: ItemType,
+            navigated: bool,
+            tooltip: String,
+            highlight: TextState,
+            selected: bool,
+        };
+
+        events {
+            detail_click: web_sys::CustomEvent,
+        };
+    }
+);
+
+parent_element!(item);
+
+#[derive(Copy, Clone, PartialEq, Eq, AsRefStr, StrAttribute)]
+pub enum SelectionMode {
+    None,
+    Single,
+    SingleStart,
+    SingleEnd,
+    SingleAuto,
+    Multiple,
+    Delete,
+}
