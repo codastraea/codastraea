a74f785cd7b69db9f74441ccc254c20b729503fe
Simon Bourne <simonbourne@gmail.com>
2025-05-11 15:02:37 +0100

    Bundle more UI5 components

--- packages/ui/js-bundler/main.js
+++ packages/ui/js-bundler/main.js
@@ -2,8 +2,14 @@ import { EditorView, basicSetup } from "codemirror"
 import { EditorState, EditorSelection } from "@codemirror/state"
 import { python } from "@codemirror/lang-python"
 import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Icon.js";
+import "@ui5/webcomponents/dist/Link.js";
+import "@ui5/webcomponents/dist/Menu.js";
+import "@ui5/webcomponents/dist/MenuItem.js";
+import "@ui5/webcomponents/dist/MenuSeparator.js";
 import "@ui5/webcomponents/dist/TabContainer.js";
 import "@ui5/webcomponents/dist/Tab.js";
+import "@ui5/webcomponents/dist/TabSeparator.js";
 import "@ui5/webcomponents/dist/Tree.js";
 import "@ui5/webcomponents/dist/TreeItem.js";
 import "@ui5/webcomponents/dist/TreeItemCustom.js";
