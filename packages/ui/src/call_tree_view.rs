27addb11efbab2d833bd337edbb71af11acf2bae
Simon Bourne <simonbourne@gmail.com>
2025-05-14 19:23:34 +0100

    Load new data when tree nodes are expanded

--- packages/ui/src/call_tree_view.rs
+++ packages/ui/src/call_tree_view.rs
@@ -106,10 +106,12 @@ fn internal_node(
             ])
         })
         .to_signal_vec();
-    // TODO: Do this when a tree item is expanded. Need to watch the `expanded`
-    // attribute.
-    is_expanded.set(true);
-    node_dropdown(node, node_type, actions).item_children_signal(body)
+    node_dropdown(node, node_type, actions)
+        .item_children_signal(body)
+        .observe_mutations(move |observer| {
+            clone!(is_expanded);
+            observer.expanded(move |elem| is_expanded.set(elem.has_attribute("expanded")))
+        })
 }
 
 fn leaf_node(
