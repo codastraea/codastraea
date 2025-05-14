e71be45bfc9e5249623a1a95da9a69ffee4a539f
Simon Bourne <simonbourne@gmail.com>
2025-05-14 14:54:31 +0100

    `xtask serve` and `xtask ui`

--- packages/xtask/src/main.rs
+++ packages/xtask/src/main.rs
@@ -1,18 +1,39 @@
+use clap::Parser;
 use xtask_base::{
     ci::{StandardVersions, CI},
-    CommonCmds,
+    cmd, in_workspace, CommonCmds,
 };
 
+#[derive(Parser)]
+enum Commands {
+    UI,
+    Serve,
+    #[clap(flatten)]
+    Common(CommonCmds),
+}
+
 fn main() {
-    CommonCmds::run(
-        CI::standard_workflow(
-            StandardVersions {
-                rustc_stable_version: "1.85.1",
-                rustc_nightly_version: "nightly-2025-03-15",
-                udeps_version: "0.1.55",
-            },
-            &[],
-        ),
-        |_| Ok(()),
-    )
+    in_workspace(|workspace| {
+        type Cmds = Commands;
+
+        match Cmds::parse() {
+            Cmds::UI => cmd!("trunk serve --open").dir("packages/ui").run()?,
+            Cmds::Serve => cmd!("cargo run").dir("packages/server").run()?,
+            Cmds::Common(cmds) => cmds.sub_command::<Cmds>(
+                workspace,
+                [],
+                CI::standard_workflow(
+                    StandardVersions {
+                        rustc_stable_version: "1.85.1",
+                        rustc_nightly_version: "nightly-2025-03-15",
+                        udeps_version: "0.1.55",
+                    },
+                    &[],
+                ),
+                |_| Ok(()),
+            )?,
+        }
+
+        Ok(())
+    });
 }
