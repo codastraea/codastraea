6d762375c12edbec03003ef932ac5ad9211190be
Simon Bourne <simonbourne@gmail.com>
2025-05-20 18:59:39 +0100

    Try and fix build

--- packages/wasm-guest/src/lib.rs
+++ packages/wasm-guest/src/lib.rs
@@ -1,17 +1,2 @@
-#![cfg(target_arch = "wasm32")]
-
-// "env" is the default anyway.
-#[link(wasm_import_module = "env")]
-unsafe extern "C" {
-    fn host_func(param: i32);
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn hello() {
-    // Release mode builds will optimize this out, so use debug mode to see it's
-    // effects
-    let _ = Box::leak(Box::new(1));
-    unsafe {
-        host_func(1);
-    }
-}
+#[cfg(target_arch = "wasm32")]
+pub mod wasm;
