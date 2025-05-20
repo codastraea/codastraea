d895976be8e4d96dd1afd24dd77488e4e18f6fd6
Simon Bourne <simonbourne@gmail.com>
2025-05-20 20:12:08 +0100

    Better names

--- packages/wasm-host/src/instrument.rs
+++ packages/wasm-host/src/instrument.rs
@@ -6,7 +6,7 @@ use walrus::{ExportItem, Global, Memory, Module, ModuleExports, Result};
 pub fn instrument(wasm_module: &[u8]) -> Result<Vec<u8>> {
     let mut module = Module::from_buffer(wasm_module)?;
 
-    export_private_items(
+    export_internal_items(
         &mut module.exports,
         "global",
         module.globals.iter().map(Global::id),
@@ -16,7 +16,7 @@ pub fn instrument(wasm_module: &[u8]) -> Result<Vec<u8>> {
         },
     );
 
-    export_private_items(
+    export_internal_items(
         &mut module.exports,
         "memory",
         module.memories.iter().map(Memory::id),
@@ -29,7 +29,7 @@ pub fn instrument(wasm_module: &[u8]) -> Result<Vec<u8>> {
     Ok(module.emit_wasm())
 }
 
-fn export_private_items<T>(
+fn export_internal_items<T>(
     exports: &mut ModuleExports,
     type_name: &str,
     ids: impl Iterator<Item = Id<T>>,
@@ -37,10 +37,10 @@ fn export_private_items<T>(
 ) where
     Id<T>: Into<ExportItem>,
 {
-    let already_exported: HashSet<Id<T>> = exports.iter().filter_map(|e| filter(e.item)).collect();
-    let private_ids = ids.filter(move |id| !already_exported.contains(id));
+    let exported_ids: HashSet<Id<T>> = exports.iter().filter_map(|e| filter(e.item)).collect();
+    let internal_ids = ids.filter(|id| !exported_ids.contains(id));
 
-    for id in private_ids {
+    for id in internal_ids {
         let name = format!("__enhedron_{type_name}_{}", id.index());
         exports.add(&name, id.into());
     }
