9239c697ab2419e88b83a4f4bac5344b68e7a24c
Simon Bourne <simonbourne@gmail.com>
2025-05-26 11:14:46 +0100

    Better `workflow` macro error reporting

--- packages/wasm-guest-proc-macro/src/lib.rs
+++ packages/wasm-guest-proc-macro/src/lib.rs
@@ -1,25 +1,41 @@
-use proc_macro::TokenStream;
-use proc_macro2::Span;
+use proc_macro2::{Span, TokenStream};
 use quote::quote;
 use syn::{
     fold::{fold_block, Fold},
-    parse_macro_input, parse_quote, ExprIf, Ident, ItemFn,
+    parse_macro_input, parse_quote, Error, ExprIf, Generics, Ident, ItemFn, Result,
 };
 
 #[proc_macro_attribute]
-pub fn workflow(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let ItemFn {
+pub fn workflow(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    impl_workflow(parse_macro_input!(item))
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+fn impl_workflow(
+    ItemFn {
         attrs,
         vis,
         sig,
         block,
-    } = parse_macro_input!(item);
+    }: ItemFn,
+) -> Result<TokenStream> {
     let ident = &sig.ident;
     let name = &ident.to_string();
     let exported_ident = Ident::new(&format!("__enhedron_ident_{name}"), Span::call_site());
     let block = fold_block(&mut Instrument, *block);
+    let generics = &sig.generics;
 
-    quote! {
+    fold_errors([
+        check_empty(generics, generics.const_params(), "`const`"),
+        check_empty(generics, generics.lifetimes(), "lifetime"),
+        check_empty(generics, generics.type_params(), "type"),
+    ])?;
+
+    Ok(quote! {
         #(#attrs)*
         #vis #sig {
             let __enhedron_trace = ::serpent_automation_wasm_guest::TraceFn::new(
@@ -41,8 +57,35 @@ pub fn workflow(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 #exported_ident
             )
         );
+    })
+}
+
+fn fold_errors(errors: impl IntoIterator<Item = Result<()>>) -> Result<()> {
+    #[allow(clippy::manual_try_fold)]
+    errors
+        .into_iter()
+        .fold(Ok(()), |left, right| match (left, right) {
+            (Err(e1), Err(mut e2)) => {
+                e2.combine(e1);
+                Err(e2)
+            }
+            (r1, r2) => r2.and(r1),
+        })
+}
+
+fn check_empty<T>(
+    generics: &Generics,
+    mut iter: impl Iterator<Item = T>,
+    name: &str,
+) -> Result<()> {
+    if iter.next().is_some() {
+        Err(Error::new_spanned(
+            generics,
+            format!("There should be no {name} parameters",),
+        ))
+    } else {
+        Ok(())
     }
-    .into()
 }
 
 struct Instrument;
