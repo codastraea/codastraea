a8d7a6607b6d70fe0b22b8606ea78b4402a4f0d8
Simon Bourne <simonbourne@gmail.com>
2025-05-28 18:11:52 +0100

    Start implementing `CallTree`

---
+++ packages/wasm-host/src/call_tree.rs
@@ -0,0 +1,69 @@
+use futures_signals::signal_vec::{MutableVec, SignalVec, SignalVecExt};
+
+pub struct CallTree {
+    children: MutableVec<Node>,
+}
+
+impl CallTree {
+    pub fn empty() -> Self {
+        Self {
+            children: MutableVec::new(),
+        }
+    }
+
+    // TODO: pub(crate)
+    // TODO: Need to think about how to track the currently running node.
+    pub fn fn_begin(&self, name: String) {
+        self.children.lock_mut().push_cloned(Node {
+            name,
+            status: Status::Running,
+            sub_tree: Self::empty(),
+        })
+    }
+
+    pub fn watch(&self, path: &[usize]) -> impl SignalVec<Item = NodeUpdate> {
+        if let Some((head, tail)) = path.split_first() {
+            self.children.lock_ref()[*head].sub_tree.watch(tail)
+        } else {
+            self.children.signal_vec_cloned().map(|node| NodeUpdate {
+                name: node.name,
+                status: node.status,
+                has_children: node.sub_tree.children.lock_ref().is_empty(),
+            })
+        }
+    }
+}
+
+struct Node {
+    name: String,
+    status: Status,
+    sub_tree: CallTree,
+}
+
+impl Clone for Node {
+    fn clone(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            status: self.status,
+            sub_tree: CallTree {
+                children: self.sub_tree.children.clone(),
+            },
+        }
+    }
+}
+
+// This could be more efficient, as we mostly update `status`. `name` never
+// changes, so only needs to be sent when we add a node.
+#[derive(Clone)]
+pub struct NodeUpdate {
+    pub name: String,
+    pub status: Status,
+    pub has_children: bool,
+}
+
+#[derive(Copy, Clone)]
+pub enum Status {
+    NotRun,
+    Running,
+    Complete,
+}
--- packages/wasm-host/src/lib.rs
+++ packages/wasm-host/src/lib.rs
@@ -1,3 +1,6 @@
+pub mod call_tree;
 pub mod instrument;
 pub mod runtime;
 pub mod snapshot;
+
+pub use call_tree::CallTree;
