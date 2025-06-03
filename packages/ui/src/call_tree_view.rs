3f44f848e4370bd3287e4e0357520fa7005a5472
Simon Bourne <simonbourne@gmail.com>
2025-06-03 13:25:57 +0100

    CallTreeView: Use streaming updates from server

--- packages/ui/src/call_tree_view.rs
+++ packages/ui/src/call_tree_view.rs
@@ -1,11 +1,14 @@
+use std::{pin::pin, rc::Rc};
+
 use derive_more::Into;
+use futures::StreamExt;
 use futures_signals::{
-    signal::SignalExt,
+    signal::{Mutable, SignalExt},
     signal_vec::{MutableVec, MutableVecLockMut, SignalVecExt},
 };
 use gloo_console::info;
 use serpent_automation_frontend::ServerConnection;
-use serpent_automation_server_api::{NodeStatus, NodeUpdate, SrcSpan, WatchCallTree};
+use serpent_automation_server_api::{NodeStatus, NodeUpdate, NodeVecDiff, SrcSpan, WatchCallTree};
 use silkenweb::{
     clone,
     node::{element::TextParentElement, Node},
@@ -27,6 +30,22 @@ pub trait CallTreeActions: Clone + 'static {
     fn view_code(&self, span: SrcSpan);
 }
 
+struct NodeData {
+    name: String,
+    status: Mutable<NodeStatus>,
+    has_children: bool,
+}
+
+impl NodeData {
+    fn from_update(value: NodeUpdate) -> Rc<Self> {
+        Rc::new(Self {
+            name: value.name,
+            status: Mutable::new(value.status),
+            has_children: value.has_children,
+        })
+    }
+}
+
 impl CallTreeView {
     pub fn new(server: ServerConnection, actions: impl CallTreeActions) -> Self {
         Self(
@@ -44,15 +63,16 @@ impl CallTreeView {
     fn node(
         server: &ServerConnection,
         mut path: Vec<usize>,
-        update: &NodeUpdate,
+        data: &Rc<NodeData>,
         actions: impl CallTreeActions,
     ) -> tree::CustomItem {
-        let node = node_dropdown(update, actions.clone());
-        // TODO: pass this around in `NodeUpdate`
+        let node = node_dropdown(data, actions.clone());
+        // TODO: pass this around in `NodeData`, or use a node_id (slot map id) rather
+        // than path?
         path.push(0);
 
-        if update.has_children {
-            let children = MutableVec::<NodeUpdate>::new();
+        if data.has_children {
+            let children = MutableVec::<Rc<NodeData>>::new();
             node.item_children_signal(children.signal_vec_cloned().map({
                 clone!(server, path);
                 move |c| Self::node(&server, path.clone(), &c, actions.clone())
@@ -68,17 +88,28 @@ impl CallTreeView {
                         children.lock_mut().clear();
 
                         // TODO: We need a way to cancel this before we apply the next one.
-                        spawn_local(server.watch(WatchCallTree::node(path.clone())).for_each({
-                            clone!(children);
-                            move |update| {
-                                info!("Applying update");
-                                MutableVecLockMut::apply_vec_diff(&mut children.lock_mut(), update);
-                                async {}
+                        clone!(server, path, children);
+                        spawn_local(async move {
+                            let mut updates =
+                                pin!(server.watch(WatchCallTree::node(path.clone())).await);
+
+                            while let Some(update) = updates.next().await {
+                                use NodeVecDiff as Diff;
+                                match update {
+                                    Diff::Replace(updates) => children.lock_mut().replace_cloned(
+                                        updates.into_iter().map(NodeData::from_update).collect(),
+                                    ),
+                                    Diff::Push(update) => children
+                                        .lock_mut()
+                                        .push_cloned(NodeData::from_update(update)),
+                                    Diff::SetStatus { index, status } => {
+                                        children.lock_ref()[index].status.set(status)
+                                    }
+                                }
                             }
-                        }));
+                        })
                     } else {
-                        let mut children = children.lock_mut();
-                        children.clear();
+                        children.lock_mut().clear();
                     }
                 }
             })
@@ -88,23 +119,25 @@ impl CallTreeView {
     }
 }
 
-fn node_dropdown(node: &NodeUpdate, actions: impl CallTreeActions) -> tree::CustomItem {
+fn node_dropdown(node: &NodeData, actions: impl CallTreeActions) -> tree::CustomItem {
     // TODO: `Design::Emphasized` for control flow nodes
     let design = Design::Default;
-    let run_status = node.status;
-    let icon = match run_status {
+    let run_status = &node.status;
+    let icon = run_status.signal().map(|run_status| match run_status {
         NodeStatus::NotRun => icon::base::circle_task(),
         NodeStatus::Running => icon::base::busy(),
         NodeStatus::Complete => icon::base::sys_enter(),
         // TODO:
         // NodeStatus::PredicateSuccessful(false) => icon::base::circle_task_2(),
         // NodeStatus::Failed => icon::base::error(),
-    };
-    let badge = if run_status == NodeStatus::Running {
-        Some(badge().design(BadgeDesign::AttentionDot))
-    } else {
-        None
-    };
+    });
+    let badge = run_status.signal().map(|run_status| {
+        if run_status == NodeStatus::Running {
+            Some(badge().design(BadgeDesign::AttentionDot))
+        } else {
+            None
+        }
+    });
 
     let menu = menu::container().item_child(
         menu::item()
@@ -114,10 +147,10 @@ fn node_dropdown(node: &NodeUpdate, actions: impl CallTreeActions) -> tree::Cust
     let button = button()
         .design(design)
         .text(&node.name)
-        .icon(icon)
+        .icon(Sig(icon))
         .end_icon(icon::base::slim_arrow_down())
         .menu_opener(&menu)
-        .badge_optional_child(badge);
+        .badge_optional_child(Sig(badge));
     tree::custom_item()
         .content_child(button)
         .content_child(menu)
