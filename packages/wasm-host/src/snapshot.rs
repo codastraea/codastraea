baa462eb6c7d622d5980df570cf62ed59a7ac8f4
Simon Bourne <simonbourne@gmail.com>
2025-06-05 15:33:38 +0100

    Snapshots: tidy

--- packages/wasm-host/src/snapshot.rs
+++ packages/wasm-host/src/snapshot.rs
@@ -6,7 +6,7 @@ use std::{
 
 use anyhow::{bail, Context, Result};
 use flate2::{read::DeflateDecoder, write::DeflateEncoder, Compression};
-use wasmtime::{AsContextMut, Instance, Memory, Ref, Val};
+use wasmtime::{AsContextMut, Global, Instance, Memory, Ref, Table, Val};
 
 // # TODO
 //
@@ -22,112 +22,53 @@ use wasmtime::{AsContextMut, Instance, Memory, Ref, Val};
 //   memory? All threads should at least be stopped before a snapshot.
 
 pub struct Snapshot {
-    global_i32s: NamedVec<i32>,
-    global_i64s: NamedVec<i64>,
-    global_f32s: NamedVec<f32>,
-    global_f64s: NamedVec<f64>,
-    global_v128s: NamedVec<u128>,
+    globals: Globals,
     memories: NamedVec<SnapshotMemory>,
     tables: NamedVec<Vec<Option<String>>>,
 }
 
-type NamedVec<T> = Vec<(String, T)>;
-
-struct SnapshotMemory {
-    page_size: u64,
-    uncompressed_len: usize,
-    data: Vec<u8>,
+#[derive(Default)]
+struct Globals {
+    i32s: NamedVec<i32>,
+    i64s: NamedVec<i64>,
+    f32s: NamedVec<f32>,
+    f64s: NamedVec<f64>,
+    v128s: NamedVec<u128>,
 }
 
-impl Snapshot {
-    pub fn new(ctx: &mut impl AsContextMut, instance: &Instance) -> Result<Self> {
-        // TODO: Break this function up
-        let mut global_i32s = Vec::new();
-        let mut global_i64s = Vec::new();
-        let mut global_f32s = Vec::new();
-        let mut global_f64s = Vec::new();
-        let mut global_v128s = Vec::new();
-        let mut memories = Vec::new();
-        let mut tables = Vec::new();
+impl Globals {
+    fn snapshot(&mut self, ctx: &mut impl AsContextMut, name: &str, global: Global) -> Result<()> {
+        if global.ty(&mut *ctx).mutability().is_var() {
+            let name = name.to_string();
 
-        let exported_names: Vec<String> = instance
-            .exports(&mut *ctx)
-            .map(|e| e.name().to_string())
-            .collect();
-
-        let lookup_func_name = func_name_lookup(ctx, instance, &exported_names);
-
-        for name in exported_names {
-            if let Some(global) = instance.get_global(&mut *ctx, &name) {
-                if global.ty(&mut *ctx).mutability().is_var() {
-                    let name = name.clone();
-
-                    match global.get(&mut *ctx) {
-                        Val::I32(val) => global_i32s.push((name, val)),
-                        Val::I64(val) => global_i64s.push((name, val)),
-                        Val::F32(val) => global_f32s.push((name, f32::from_bits(val))),
-                        Val::F64(val) => global_f64s.push((name, f64::from_bits(val))),
-                        Val::V128(val) => global_v128s.push((name, val.as_u128())),
-                        Val::FuncRef(_) => {
-                            bail!("Global '{name}': Mutable `FuncRef`s are not supported")
-                        }
-                        Val::ExternRef(_) => {
-                            bail!("Global '{name}': Mutable `ExternRef`s are not supported")
-                        }
-                        Val::AnyRef(_) => {
-                            bail!("Global '{name}': Mutable `AnyRef`s are not supported")
-                        }
-                    }
+            match global.get(&mut *ctx) {
+                Val::I32(val) => self.i32s.push((name, val)),
+                Val::I64(val) => self.i64s.push((name, val)),
+                Val::F32(val) => self.f32s.push((name, f32::from_bits(val))),
+                Val::F64(val) => self.f64s.push((name, f64::from_bits(val))),
+                Val::V128(val) => self.v128s.push((name, val.as_u128())),
+                Val::FuncRef(_) => {
+                    bail!("Global '{name}': Mutable `FuncRef`s are not supported")
                 }
-            }
-
-            if let Some(memory) = instance.get_memory(&mut *ctx, &name) {
-                snapshot_memory(ctx, &mut memories, &name, memory)?;
-            }
-
-            if let Some(table) = instance.get_table(&mut *ctx, &name) {
-                let mut table_data = Vec::new();
-
-                for index in 0..table.size(&*ctx) {
-                    let item = table
-                        .get(&mut *ctx, index)
-                        .expect("Index should be in bounds");
-
-                    let item = match item {
-                        Ref::Func(None) => None,
-                        Ref::Func(Some(func)) => {
-                            let name = lookup_func_name
-                                .get(&unsafe { func.to_raw(&mut *ctx) })
-                                .with_context(|| {
-                                    format!("Table {name}[{index}]: function not found")
-                                })?;
-
-                            Some(name.clone())
-                        }
-                        Ref::Extern(_) => {
-                            bail!("Table {name}[{index}]: `ExternRef`s are not supported")
-                        }
-                        Ref::Any(_) => {
-                            bail!("Table {name}[{index}]: `AnyRef`s are not supported")
-                        }
-                    };
-
-                    table_data.push(item);
+                Val::ExternRef(_) => {
+                    bail!("Global '{name}': Mutable `ExternRef`s are not supported")
                 }
-
-                tables.push((name.clone(), table_data));
+                Val::AnyRef(_) => {
+                    bail!("Global '{name}': Mutable `AnyRef`s are not supported")
+                }
             }
         }
 
-        Ok(Self {
-            global_i32s,
-            global_i64s,
-            global_f32s,
-            global_f64s,
-            global_v128s,
-            memories,
-            tables,
-        })
+        Ok(())
+    }
+
+    fn restore(&self, ctx: &mut impl AsContextMut, instance: &Instance) -> Result<()> {
+        Self::set_globals(ctx, instance, &self.i32s)?;
+        Self::set_globals(ctx, instance, &self.i64s)?;
+        Self::set_globals(ctx, instance, &self.f32s)?;
+        Self::set_globals(ctx, instance, &self.f64s)?;
+        Self::set_globals(ctx, instance, &self.v128s)?;
+        Ok(())
     }
 
     fn set_globals<T: Copy + Into<Val>>(
@@ -145,9 +86,59 @@ impl Snapshot {
 
         Ok(())
     }
+}
+
+type NamedVec<T> = Vec<(String, T)>;
+
+struct SnapshotMemory {
+    page_size: u64,
+    uncompressed_len: usize,
+    data: Vec<u8>,
+}
+
+impl Snapshot {
+    pub fn new(ctx: &mut impl AsContextMut, instance: &Instance) -> Result<Self> {
+        let mut globals = Globals::default();
+        let mut memories = Vec::new();
+        let mut tables = Vec::new();
+
+        let exported_names: Vec<String> = instance
+            .exports(&mut *ctx)
+            .map(|e| e.name().to_string())
+            .collect();
+
+        let lookup_func_name = func_name_lookup(ctx, instance, &exported_names);
+
+        for name in exported_names {
+            if let Some(global) = instance.get_global(&mut *ctx, &name) {
+                globals.snapshot(ctx, &name, global)?;
+            }
+
+            if let Some(memory) = instance.get_memory(&mut *ctx, &name) {
+                memories.push((
+                    name.clone(),
+                    snapshot_memory(ctx, memory).with_context(|| format!("Memory {name}"))?,
+                ));
+            }
+
+            if let Some(table) = instance.get_table(&mut *ctx, &name) {
+                tables.push((
+                    name.clone(),
+                    snapshot_table(ctx, &lookup_func_name, table)
+                        .with_context(|| format!("Table {name}"))?,
+                ));
+            }
+        }
+
+        Ok(Self {
+            globals,
+            memories,
+            tables,
+        })
+    }
 
     pub fn restore(&self, ctx: &mut impl AsContextMut, instance: &Instance) -> Result<()> {
-        self.restore_globals(ctx, instance)?;
+        self.globals.restore(ctx, instance)?;
         self.restore_tables(ctx, instance)?;
         self.restore_memories(ctx, instance)?;
 
@@ -219,36 +210,48 @@ impl Snapshot {
 
         Ok(())
     }
-
-    fn restore_globals(&self, ctx: &mut impl AsContextMut, instance: &Instance) -> Result<()> {
-        Self::set_globals(ctx, instance, &self.global_i32s)?;
-        Self::set_globals(ctx, instance, &self.global_i64s)?;
-        Self::set_globals(ctx, instance, &self.global_f32s)?;
-        Self::set_globals(ctx, instance, &self.global_f64s)?;
-        Self::set_globals(ctx, instance, &self.global_v128s)?;
-        Ok(())
-    }
 }
 
-fn snapshot_memory(
+fn snapshot_table(
     ctx: &mut impl AsContextMut,
-    memories: &mut NamedVec<SnapshotMemory>,
-    name: &str,
-    memory: Memory,
-) -> Result<()> {
+    lookup_func_name: &HashMap<*mut c_void, String>,
+    table: Table,
+) -> Result<Vec<Option<String>>> {
+    let mut table_data = Vec::new();
+    for index in 0..table.size(&*ctx) {
+        let item = table
+            .get(&mut *ctx, index)
+            .expect("Index should be in bounds");
+
+        let item = match item {
+            Ref::Func(None) => None,
+            Ref::Func(Some(func)) => {
+                let name = lookup_func_name
+                    .get(&unsafe { func.to_raw(&mut *ctx) })
+                    .context("Function not found")?;
+
+                Some(name.clone())
+            }
+            Ref::Extern(_) => bail!("`ExternRef`s are not supported"),
+            Ref::Any(_) => bail!("`AnyRef`s are not supported"),
+        };
+
+        table_data.push(item);
+    }
+
+    Ok(table_data)
+}
+
+fn snapshot_memory(ctx: &mut impl AsContextMut, memory: Memory) -> Result<SnapshotMemory> {
     let mut compressor = DeflateEncoder::new(Vec::new(), Compression::default());
     compressor.write_all(memory.data(&mut *ctx))?;
     let uncompressed_len = memory.data_size(&mut *ctx);
     let data = compressor.finish()?;
-    memories.push((
-        name.to_string(),
-        SnapshotMemory {
-            page_size: memory.page_size(&mut *ctx),
-            uncompressed_len,
-            data,
-        },
-    ));
-    Ok(())
+    Ok(SnapshotMemory {
+        page_size: memory.page_size(&mut *ctx),
+        uncompressed_len,
+        data,
+    })
 }
 
 fn func_name_lookup(
